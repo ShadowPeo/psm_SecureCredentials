b08b6e2a1ec6b85e2d4377fa8fcb8021830822e8
= <=>
2024-03-07 15:44:50 +1100

    Changed some variable names for easier use (shorter) Added function definition for creating an AES Keyfile
     Changes to be committed:
    	modified:   secureCredentials.ps1

--- secureCredentials.ps1
+++ secureCredentials.ps1
@@ -11,17 +11,26 @@ Create Creds
     Save credential as (credIdentifier-?)(DeviceName-(if DPAPI))Username
 #>
 
+function New-CredentialKey
+{
+    Param 
+    (
+        [string]$keyStore = "$HOME\.Keys",            # Allows oferriding of the default keystore location
+        [string]$keyFile                              # If there is either a / or a \ in the file it will assume that its a full path, if there are no slashes it will assume its a reletive path to the keystore. If this is not specified it will be assumed that the file matches the username or if not matches the device name
+    )        
+}
+
 function New-secureCredential
 {
     Param 
     (
-        [switch]$aesOnly,                                           # Use AES Only, do not use DPAPI
-        [string(Mandatory=$true)]$username,                         # Username for the credential
-        [string]$rootStore = "$($ENV:USERPROFILE)\.PSCreds",        # Allows overriding of the default credential root location
-        [string]$keyStore = "$($ENV:USERPROFILE)\.Keys",            # Allows oferriding of the default keystore location
-        [string]$credentialIdentifer,                               # Used to identify a unique credential where the credential is unique for a given script/task, if not set generic credentials for username will be used. Recommended input is the script name
-        [string]$credentialFile,                                    # Credential File, this needs to be the full path to the file
-        [string]$keyFile                                            # Full path to a specifc Keyfile, if this does not exist it will be created and used to encrypt the data, if it does exist, this key will be used
+        [switch]$aesOnly,                             # Use AES Only, do not use DPAPI
+        [string(Mandatory=$true)]$username,           # Username for the credential
+        [string]$rootStore = "$HOME\.PSCreds",        # Allows overriding of the default credential root location
+        [string]$keyStore = "$HOME\.Keys",            # Allows oferriding of the default keystore location
+        [string]$credIdentifer,                 # Used to identify a unique credential where the credential is unique for a given script/task, if not set generic credentials for username will be used. Recommended input is the script name
+        [string]$credFile,                      # Credential File, this needs to be the full path to the file
+        [string]$keyFile                              # If there is either a / or a \ in the file it will assume that its a full path, if there are no slashes it will assume its a reletive path to the keystore
     )
 
     #Test Paths, Create if do not exist
@@ -36,11 +45,11 @@ function New-secureCredential
 
     }
 
-    # Calculate Credential File Name if not specified in the $credentialFile Option
+    # Calculate Credential File Name if not specified in the $credFile Option
 
-    if ([string]::IsNullOrEmpty($credentialFile))
+    if ([string]::IsNullOrEmpty($credFile))
     {
-        $credentialFile = "$(if(-not [string]::IsNullOrEmpty("$credentialIdentifer-")){$credentialIdentifer})$(if(-not $shared){"$($ENV:COMPUTERNAME)-"})$username.crd"
+        $credFile = "$(if(-not [string]::IsNullOrEmpty("$credIdentifer-")){$credIdentifer})$(if(-not $shared){"$($ENV:COMPUTERNAME)-"})$username.crd"
     }
 }
 
@@ -49,14 +58,14 @@ function Get-secureCredential
 {
     Param 
     (
-        [switch]$aesOnly,                                           # Use AES Only, do not use DPAPI
-        [string(Mandatory=$true)]$username,                         # Username for the credential
-        [string]$rootStore = "$($ENV:USERPROFILE)\.PSCreds",        # Allows overriding of the default credential root location
-        [string]$keyStore = "$($ENV:USERPROFILE)\.Keys",            # Allows oferriding of the default keystore location
-        [string]$credentialIdentifer,                               # Used to identify a unique credential where the credential is unique for a given script/task, if not set generic credentials for username will be used. Recommended input is the script name
-        [string]$credentialFile,                                    # Credential File, this needs to be the full path to the file
-        [string]$keyFile,                                           # Full path to a specifc Keyfile, if this does not exist it will be created and used to encrypt the data, if it does exist, this key will be used
-        [switch]$createOnNotFound                                   # Ask to create the credential if not found
+        [switch]$aesOnly,                             # Use AES Only, do not use DPAPI
+        [string(Mandatory=$true)]$username,           # Username for the credential
+        [string]$rootStore = "$HOME\.PSCreds",        # Allows overriding of the default credential root location
+        [string]$keyStore = "$HOME\.Keys",            # Allows oferriding of the default keystore location
+        [string]$credIdentifer,                 # Used to identify a unique credential where the credential is unique for a given script/task, if not set generic credentials for username will be used. Recommended input is the script name
+        [string]$credFile,                      # Credential File, this needs to be the full path to the file
+        [string]$keyFile,                             # If there is either a / or a \ in the file it will assume that its a full path, if there are no slashes it will assume its a reletive path to the keystore
+        [switch]$createOnNotFound                     # Ask to create the credential if not found
     )
 
 
